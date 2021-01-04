8525e4a89494ea66cfa7b35569c4d68d554249d7
Jessica Södereng <jessicasodereng@Jessicas-MBP.lan>
2021-01-04 17:36:14 +0100

    Fixed with images, dropdown functions, back buttons

--- Project/script.js
+++ Project/script.js
@@ -17,21 +17,12 @@ $("document").ready(function () {
 
     //Sorteringsknapp kategorisida
     $(".sort-button").click(function () {
-        $(".dropdown-content").slideDown(200);
-        $(this).find("i").removeClass("fas fa-chevron-down").addClass("fas fa-chevron-up"); //ändrar pilen
-    });
-
-    $(".popular-sort").click(function() {
-        //Visa alla produkter i den "ordinarie" ordningen
-    });
-
-    $(".lowest-price-sort").click(function() {
-        //Visa produkter i stigande prisordning
-        $()
-    });
-
-    $(".highest-price-sort").click(function() {
-        //Visa produkter i stigande prisordning
+        $(".dropdown-content").toggleClass("show-sort");
+        if ($(".dropdown-content").hasClass(("show-sort"))) {
+            $(".sort-button").find("i").removeClass("fas fa-chevron-down").addClass("fas fa-chevron-up");
+        } else {
+            $(".sort-button").find("i").removeClass("fas fa-chevron-up").addClass("fas fa-chevron-down");
+        };
     });
 
     //Ändrar bilden som man klickar på, på produktsidan
@@ -76,25 +67,29 @@ $("document").ready(function () {
 
     //Dropdown meny med färgalternativ produktsida
     $(".color-button").click(function () {
-        $(".dropdown-color-content").slideDown(200);
-        $(this).find("i").removeClass("fas fa-chevron-down").addClass("fas fa-chevron-up"); //ändrar pilen
+        $(".dropdown-color-content").toggleClass("show-dropdown-color-content");
+        if ($(".dropdown-color-content").hasClass(("show-dropdown-color-content"))) {
+            $(".color-button").find("i").removeClass("fas fa-chevron-down").addClass("fas fa-chevron-up");
+        } else {
+            $(".color-button").find("i").removeClass("fas fa-chevron-up").addClass("fas fa-chevron-down");
+        };
     });
 
     $("#color-1").click(function () {
-        //När man väljer ny färg i dropdown menyn - ändra produktbilderna
         $(".main-image img").attr("src", $(this).attr("src").replace());
         $("#image-1").attr("src", $(this).attr("src").replace());
         $("#image-2").attr("src", $(this).attr("src").replace(".jpg", "-profil.jpg"));
         $("#image-3").attr("src", $(this).attr("src").replace(".jpg", "-rum.jpg"));
         $("#output").text($("#output").attr("default"));
+        $(".dropdown-color-content").toggleClass("show-dropdown-color-content");
     });
     $("#color-2").click(function () {
-        //När man väljer ny färg i dropdown menyn - ändra produktbilderna
         $(".main-image img").attr("src", $(this).attr("src").replace());
         $("#image-1").attr("src", $(this).attr("src").replace());
         $("#image-2").attr("src", $(this).attr("src").replace(".jpg", "-profil.jpg"));
         $("#image-3").attr("src", $(this).attr("src").replace(".jpg", "-rum.jpg"));
         $("#output").text($("#output").attr("default"));
+        $(".dropdown-color-content").toggleClass("show-dropdown-color-content");
     });
 
     $("#increase").click(function () {
@@ -112,30 +107,25 @@ $("document").ready(function () {
     });
 
     $(".add-button").click(function () {
-        /* Lägger till i kundkorgen, måste kolla vald produkt (namn, pris), 
-        valt antal, hämta produktbild - Koppla till det som visas i kundkorgen */
+    /* 1. Lägger till i varukorg och varukorg slidear ut och sen tillbaka
+        2. Lägger till vald produkt (bild, namn, pris, antal)
+        3. Totalpris längst ner 
+        4. Lägger till knappar för justering av volym (som på produktsidan) */
+
+    });
+
+    $(".back-button").click(function() {
+        $(".main-page").css("display", "none");
+        $(".product-page").css("display", "none");
+        $(".category-page").css("display", "block");
+    });
+
+    $(".checkout-button").click(function() {
+        $(".shopping-cart").toggleClass("shopping-cart--open");
     });
 
     $("#show-menu").click(function () {
         $(".side-menu").toggleClass("side-menu--open");
-    });
-
-    $("#arrow").click(function () {
-        $(".side-menu").toggleClass("side-menu--open");
-    });
-
-    $("#show-cart").click(function () {
-        $(".shopping-cart").toggleClass("shopping-cart--open");
-
-    });
-
-    $(".filter-button").click(function () {
-        $(".filter-menu").toggleClass("filter-menu--open");
-    });
-
-    $(".slider").on("change", function (){
-        let priceValue = $(this).val();
-        $(".price-max").text(priceValue);
     });
 
     $(".category-1").click(function() {
@@ -144,5 +134,38 @@ $("document").ready(function () {
         $(".category-page").css("display", "block");
         $(".side-menu").toggleClass("side-menu--open");
     });
+
+    $(".back-start-button").click(function() {
+        $(".main-page").css("display", "block");
+        $(".product-page").css("display", "none");
+        $(".category-page").css("display", "none");
+    });
+
+    $("#arrow").click(function () {
+        $(".side-menu").toggleClass("side-menu--open");
+    });
+
+    $("#show-cart").click(function () {
+        $(".shopping-cart").toggleClass("shopping-cart--open");
+    });
+
+    $(".filter-button").click(function () {
+        $(".filter-menu").toggleClass("filter-menu--open");
+    });
+
+    $(".color-circle").click(function() {
+        $(".color-circle").css("border", "solid 1px rgb(206, 206, 206)");
+        $(this).css("border", "solid rgb(100,100,100)");
+    })
+
+    $(".slider").on("change", function (){
+        let priceValue = $(this).val();
+        $(".price-max").text(priceValue);
+    });
+
+    $(".save-filter").click(function() {
+        $(".filter-menu").toggleClass("filter-menu--open");
+    });
+    
 
 });
